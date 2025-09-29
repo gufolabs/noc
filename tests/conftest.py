1b8b269bc498f68a4b499c10e9d0be51fc2c88cb
Dmitry Volodin <dv@nocproject.org>
2025-09-29 21:41:37 +0200

    tests: Move fixture loading into conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,21 @@
 
 # Python modules
 from collections import defaultdict
-from typing import DefaultDict, Dict
+from typing import DefaultDict, Dict, List, Any
 from time import perf_counter_ns
-from typing import List
+import functools
 
 # Third-party modules
 import pytest
-import pymongo
+from fs import open_fs
+import orjson
+from django.db import models
+
 
 # NOC modules
 from noc.config import config
+from noc.models import get_model, is_document
+from noc.core.model.fields import DocumentReferenceField, CachedForeignKey
 
 
 _stats = None
@@ -51,6 +56,28 @@ def pytest_runtest_teardown(item: pytest.Item, nextitem: pytest.Item):
     func_name: str = item.originalname or item.name.split("[")[0]
     _durations[func_name] += duration
     _counts[func_name] += 1
+
+
+def with_timing(name: str):
+    """Measure execution time of wrapped function."""
+
+    def inner(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            start = perf_counter_ns()
+            try:
+                return fn(*args, **kwargs)
+            except Exception as e:
+                _durations[name] += perf_counter_ns() - start
+                _counts[name] += 1
+                pytest.exit(f"{name} failed: {e}")
+            finally:
+                _durations[name] += perf_counter_ns() - start
+                _counts[name] += 1
+
+        return wrapper
+
+    return inner
 
 
 NS = 1_000_000_000.0
@@ -127,9 +154,11 @@ def database(request, db_postgres, db_mongo, db_clickhouse, db_kafka):
     _ensure_indexes()
     _load_collections()
     _load_mibs()
+    _load_fixtures()
     yield
 
 
+@with_timing("create_pg_db")
 def _create_pg_db():
     """Create postgres test database."""
     import psycopg2
@@ -155,24 +184,24 @@ def _create_pg_db():
             )
             row = cursor.fetchone()
             if not row:
-                pytest.exit(f"Database {database} does not exist", returncode=1)
+                msg = f"Database {database} does not exist"
+                raise RuntimeError(msg)
 
 
+@with_timing("create_mongo_db")
 def _create_mongo_db():
     """Create mongodb test database."""
     # MongoDB creates database automatically on connect
     from noc.core.mongo.connection import get_db
 
-    try:
-        db = get_db()
-        coll_name = "__test"
-        coll = db[coll_name]
-        coll.insert_one({"ping": 1})
-    except pymongo.errors.OperationFailure as e:
-        pytest.exit(f"Failed to connect mongodb: {e}", returncode=1)
+    db = get_db()
+    coll_name = "__test"
+    coll = db[coll_name]
+    coll.insert_one({"ping": 1})
     doc = coll.find_one({})
     if not doc or "ping" not in doc or doc["ping"] != 1:
-        pytest.exit("Mongodb check failed: record insertion failed")
+        msg = "Mongodb check failed: record insertion failed"
+        raise RuntimeError(msg)
 
 
 def _create_clickhouse_db():
@@ -199,45 +228,112 @@ def _drop_kafka_db():
     """Drop kafka database."""
 
 
+@with_timing("migrate_db")
 def _migrate_db():
     m = __import__("noc.commands.migrate", {}, {}, "Command")
     r = m.Command().run_from_argv([])
     if r:
-        pytest.exit("Failed to migrate database")
+        raise RuntimeError("Failed to migrate database")
 
 
+@with_timing("migrate_kafka")
 def _migrate_kafka():
     m = __import__("noc.commands.migrate-liftbridge", {}, {}, "Command")
     r = m.Command().run_from_argv(["--slots", "1"])
     if r:
-        pytest.exit("Failed to create Kafka topics")
+        raise RuntimeError("Failed to create Kafka topics")
 
 
+@with_timing("migrate_clickhouse")
 def _migrate_clickhouse():
     m = __import__("noc.commands.migrate-ch", {}, {}, "Command")
     r = m.Command().run_from_argv([])
     if r:
-        pytest.exit("Failed to migrate ClickHouse database")
+        raise RuntimeError("Failed to migrate ClickHouse database")
 
 
+@with_timing("ensure_indexes")
 def _ensure_indexes():
     m = __import__("noc.commands.ensure-indexes", {}, {}, "Command")
     r = m.Command().run_from_argv([])
     if r:
-        pytest.exit("Failed to create indexes")
+        raise RuntimeError("Failed to create indexes")
 
 
+@with_timing("collection_sync")
 def _load_collections():
     from noc.core.collection.base import Collection
 
-    try:
-        Collection.sync_all()
-    except ValueError as e:
-        pytest.exit(f"Failed to load collections: {e}")
+    Collection.sync_all()
 
 
+@with_timing("load_mibs")
 def _load_mibs():
     m = __import__("noc.commands.sync-mibs", {}, {}, "Command")
     r = m.Command().run_from_argv([])
     if r:
-        pytest.exit("Failed to load MIBs")
+        raise RuntimeError("Failed to load MIBs")
+
+
+@with_timing
+def _load_fixtures():
+    for url in config.tests.fixtures_paths:
+        with open_fs(url) as fs:
+            for path in sorted(fs.walk.files(filter=["*.json"])):
+                with fs.open(path) as f:
+                    data = orjson.loads(f.read())
+                if not isinstance(data, list):
+                    data = [data]
+                for i in data:
+                    _load_data(i)
+
+
+model_refs = {}  # model -> name -> model
+m2m_refs = {}  # model -> name -> model
+
+
+def _load_data(data: List[Dict[str, Any]]) -> None:
+    def _dereference(model, id):
+        if hasattr(model, "get_by_id"):
+            return model.get_by_id(id)
+        return model.objects.get(pk=id)
+
+    assert "$model" in data
+    model = get_model(data["$model"])
+    assert model
+    # Get reference fields
+    refs = model_refs.get(data["$model"])  # name -> model
+    mrefs = m2m_refs.get(data["$model"])  # name -> model
+    if refs is None:
+        refs = {}
+        mrefs = {}
+        if not is_document(model):
+            # Django models
+            for f in model._meta.fields:
+                if isinstance(f, (models.ForeignKey, CachedForeignKey)):
+                    refs[f.name] = f.remote_field.model
+                elif isinstance(f, DocumentReferenceField):
+                    refs[f.name] = f.document
+            for f in model._meta.many_to_many:
+                if isinstance(f, models.ManyToManyField):
+                    mrefs[f.name] = f.remote_field.model
+        model_refs[data["$model"]] = refs
+        m2m_refs[data["$model"]] = mrefs
+    kwargs = {}
+    m2m = {}
+    for k in data:
+        if k.startswith("$"):
+            continue
+        if k in refs:
+            kwargs[k] = _dereference(refs[k], data[k])
+        elif k in mrefs:
+            m2m[k] = [_dereference(mrefs[k], x) for x in data[k]]
+        else:
+            kwargs[k] = data[k]
+    d = model(**kwargs)
+    d.save()
+    assert d.pk
+    # M2M fields
+    for k in m2m:
+        for r in m2m[k]:
+            getattr(d, k).add(r)
