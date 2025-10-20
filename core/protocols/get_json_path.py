c07a641e8cb10461d94c5db51a43ae3e293d1cba
Dmitry Volodin <dv@nocproject.org>
2025-10-20 20:37:46 +0200

    GetJsonPath protocol

---
+++ core/protocols/get_json_path.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------
+# GetJsonPath protocol
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2025 Gufo Labs
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from pathlib import Path
+from typing import Protocol
+
+
+class GetJsonPath(Protocol):
+    """
+    Relative JSON path in the collection.
+
+    Models must implement GetJsonPath protocol in order
+    to save and load data from collections.
+    """
+
+    def get_json_path(self) -> Path: ...
