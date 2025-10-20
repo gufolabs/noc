042782f3b1264fcbf71214f25495db5f09c52523
Dmitry Volodin <dv@nocproject.org>
2025-10-20 20:37:46 +0200

    GetJsonPath protocol

--- tests/models/test_0009_get_json_path.py
+++ tests/models/test_0009_get_json_path.py
@@ -1,20 +1,42 @@
 # ----------------------------------------------------------------------
 # Test .get_json_path() method
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2025 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Python modules
+from typing import runtime_checkable, Set
+from pathlib import Path
 
 # Third-party modules
 import pytest
 
 # NOC modules
+from noc.core.protocols.get_json_path import GetJsonPath
 from .util import get_documents
 
 
-@pytest.mark.parametrize("model", [x for x in get_documents() if hasattr(x, "get_json_path")])
-def test_document_get_json_path(model):
+# Mark @runtime_checkable for isinstance() support
+@runtime_checkable
+class CheckGetJsonPath(GetJsonPath): ...
+
+
+SELECTED_MODELS = [x for x in get_documents() if hasattr(x, "get_json_path")]
+
+
+@pytest.mark.parametrize("model", SELECTED_MODELS)
+def test_get_json_path_protocol(model) -> None:
+    assert isinstance(model, CheckGetJsonPath)
+
+
+@pytest.mark.parametrize("model", SELECTED_MODELS)
+def test_document_get_json_path(model) -> None:
+    seen: Set[Path] = set()
     for o in model.objects.all():
         path = o.get_json_path()
         assert path
-        assert isinstance(path, str)
+        assert isinstance(path, Path)
+        assert path.suffix == ".json"
+        assert path not in seen
+        seen.add(path)
