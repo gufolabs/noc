821116258296947fb29839bed16acd8eb5edf7e3
Dmitry Volodin <dv@nocproject.org>
2025-10-22 22:23:16 +0200

    Fix

--- config.py
+++ config.py
@@ -796,8 +796,8 @@ class Config(BaseConfig):
         retry_timeout = IntParameter(default=2)
         use_proxy = BooleanParameter(default=False)
         proxy_address = StringParameter()
-        max_body_size = BytesParameter(default=3000)
-        max_file_size = BytesParameter(default=50000000)
+        max_body_size = BytesSizeParameter(default=3000)
+        max_file_size = BytesSizeParameter(default=50000000)
         http_connect_timeout = IntParameter(min=2, max=60, default=5)
         http_request_timeout = IntParameter(min=2, max=60, default=3)
 
