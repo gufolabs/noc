4f0b7b4a10124257961ee799ebae1ecfc74540de
Dmitry Volodin <dv@nocproject.org>
2025-11-07 14:23:14 +0100

    fix

--- tests/models/test_0008_to_json.py
+++ tests/models/test_0008_to_json.py
@@ -37,7 +37,7 @@ def test_get_json_path(model) -> None:
         taken_by = seen.get(path)
         if taken_by:
             print(f">>> UUID: {o.uuid} -- {o}")
-            t_o = model.object.get(id=taken_by)
+            t_o = model.objects.get(id=taken_by)
             print(f"   taken by {t_o.uuid} -- {t_o}")
         assert taken_by is None, f"{path} requested by {o.uuid} but already taken by {taken_by}"
         seen[path] = o.uuid
