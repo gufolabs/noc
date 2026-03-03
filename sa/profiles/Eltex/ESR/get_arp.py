d6144a9cfe1b54d6c694c4aa1496aa9f2ceaf6eb
Vladislav A Kulikov <vladkansk@gmail.com>
2026-03-03 13:29:30 +0300

    Add Eltex ESR MPLS Functions

--- sa/profiles/Eltex/ESR/get_arp.py
+++ sa/profiles/Eltex/ESR/get_arp.py
@@ -15,13 +15,17 @@ class Script(BaseScript):
     name = "Eltex.ESR.get_arp"
     interface = IGetARP
 
-    def execute_cli(self, interface=None):
+    def execute_cli(self, interface=None, vrf=None):
+        if vrf:
+            c = self.cli("show arp vrf %s" % vrf)
+        else:
+            c = self.cli("show arp")
         r = []
-        c = self.cli("show arp")
         for iface, ip, mac, state, age in parse_table(c):
             if (interface is not None) and (interface != iface):
                 continue
             if mac == "--":
                 mac = None
-            r += [{"ip": ip, "mac": mac, "interface": iface}]
+            if mac:
+                r += [{"ip": ip, "mac": mac, "interface": iface}]
         return r
