f67159067c873fc664ca4ff7622aa89c0c8d4ab5
Michael R <rmiclee@gmail.com>
2026-03-26 11:54:31 +0300

    Update SMG profile

--- sa/profiles/Eltex/SMG/profile.py
+++ sa/profiles/Eltex/SMG/profile.py
@@ -2,7 +2,7 @@
 # Vendor: Eltex
 # OS:     SMG
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2026 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -17,3 +17,11 @@ class Profile(BaseProfile):
     # pattern_prompt = r"^(?P<hostname>\S+)# "
     pattern_prompt = rb"(SMG2016> )|(SMG3016> )|(/[\w/]+ # )"
     command_exit = "exit"
+
+    @classmethod
+    def get_interface_type(cls, name):
+        if name.startswith("front"):
+            return "physical"
+        if name.startswith(("host", "sm", "eth")):
+            return "other"
+        raise Exception("Cannot detect interface type for %s" % name)
