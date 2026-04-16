de38a4ca2281a59f562a8f9fcf4f277d0d423d44
Michael R <rmiclee@gmail.com>
2026-04-16 10:03:05 +0300

    Add example of received version for match with regex

--- sa/profiles/Eltex/SMG/get_version.py
+++ sa/profiles/Eltex/SMG/get_version.py
@@ -18,6 +18,8 @@ class Script(BaseScript):
     def execute_snmp(self):
         v = self.snmp.get("1.3.6.1.4.1.35265.1.29.3.0", cached=True)
         if v:
+            # Example of received version info:
+            # ECSS-10 V.3.407.1.6865 3016/S/DEMO [2026-03-16 - 2026-05-31] Build: Jul 14 2025 17:45:03
             match = self.re_search(r"V\.(?P<version>\S+)\s(?P<platform>\d+)", v)
             version = match.group("version")
             platform = "SMG-" + match.group("platform")
