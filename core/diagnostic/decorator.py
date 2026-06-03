9d29d064e4dbf97015cb90cd8d9a9bb6af9d1800
Andrey Vertiprahov <aversanta@gmail.com>
2026-06-03 14:07:40 +0000

    release-25.1:Backport!10045

--- core/diagnostic/decorator.py
+++ core/diagnostic/decorator.py
@@ -6,6 +6,7 @@
 # ----------------------------------------------------------------------
 
 # Python modules
+import datetime
 from typing import List, Iterable, Optional, Dict
 
 # NOC modules
@@ -55,10 +56,9 @@ def save_document_diagnostics(
     """"""
     # Expired/Add watchers
     self.diagnostics = [DiagnosticItemDoc.from_value(d) for d in diagnostics]
-    saved, expired = [], []
+    expired: List[datetime.datetime] = []
     for d in diagnostics:
         expired.extend(c.expired for c in d.checks or [] if c.expired)
-    self.diagnostics = saved
     if expired:
         self.add_watch(ObjectEffect.DIAGNOSTIC_CHECK, after=max(expired), dry_run=dry_run)
     else:
