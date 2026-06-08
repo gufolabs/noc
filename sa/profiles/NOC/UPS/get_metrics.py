187740b6de299924249d1e6dcd45deba0135d37f
alakiza <off.lakiza@yandex.ru>
2026-06-08 10:40:26 +0300

    Add UPS monitoring with UPS-MIB

---
+++ sa/profiles/NOC/UPS/get_metrics.py
@@ -0,0 +1,48 @@
+# ---------------------------------------------------------------------
+# NOC.UPS.get_metrics
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2026 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
+from noc.core.mib import mib
+from noc.core.script.metrics import scale
+
+
+class Script(GetMetricsScript):
+    name = "NOC.UPS.get_metrics"
+
+    SENSOR_OID_SCALE = {
+        mib["UPS-MIB::upsInputFrequency", 1]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputFrequency", 2]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputFrequency", 3]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputFrequency", 1, 0]: scale(0.1, 2),
+
+        mib["UPS-MIB::upsOutputFrequency", 1]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputFrequency", 2]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputFrequency", 3]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputFrequency", 1, 0]: scale(0.1, 2),
+
+        mib["UPS-MIB::upsInputCurrent", 1]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputCurrent", 2]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputCurrent", 3]: scale(0.1, 2),
+        mib["UPS-MIB::upsInputCurrent", 1, 0]: scale(0.1, 2),
+
+        mib["UPS-MIB::upsOutputCurrent", 1]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputCurrent", 2]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputCurrent", 3]: scale(0.1, 2),
+        mib["UPS-MIB::upsOutputCurrent", 1, 0]: scale(0.1, 2),
+
+        mib["UPS-MIB::upsBypassCurrent", 1]: scale(0.1, 2),
+        mib["UPS-MIB::upsBypassCurrent", 2]: scale(0.1, 2),
+        mib["UPS-MIB::upsBypassCurrent", 3]: scale(0.1, 2),
+        mib["UPS-MIB::upsBypassCurrent", 1, 0]: scale(0.1, 2),
+
+        mib["UPS-MIB::upsEstimatedMinutesRemaining", 0]: scale(60),
+
+        mib["UPS-MIB::upsBatteryVoltage", 0]: scale(0.1, 2),
+        mib["UPS-MIB::upsBatteryCurrent", 0]: scale(0.1, 2),
+    }
+
---
+++ services/web/apps/pm/ddash/dashboards/mo_ups.py
@@ -0,0 +1,244 @@
+# ---------------------------------------------------------------------
+# DVBC dynamic dashboard
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Third-Party modules
+from collections import defaultdict
+
+# NOC modules
+from .mo import MODashboard
+from noc.inv.models.interface import Interface
+from noc.inv.models.object import Object
+from noc.inv.models.sensor import Sensor
+from noc.pm.models.metrictype import MetricType
+from noc.core.validators import is_ipv4
+
+TITLE_BAD_CHARS = '"\\\n\r'
+
+
+class UPSDashboard(MODashboard):
+    name = "moups"
+    template = "dash_moups.j2"
+    has_capability = "Device | UPS"
+
+    def get_value_mappings(self):
+        r = {
+            "upsBatteryStatus": { 
+                 "1": "unknown",
+                 "2": "batteryNormal",
+                 "3": "batteryLow",
+                 "4": "batteryDepleted",
+            },
+            "upsOutputSource": {
+                 "1": "other",
+                 "2": "none",
+                 "3": "normal",
+                 "4": "bypass",
+                 "5": "battery",
+                 "6": "booster",
+                 "7": "reducer",
+            },
+        }
+        return r
+
+    def get_value_thresholds(self):
+        r = {
+            "upsBatteryStatus" : [
+                {"color": "gray", "value": "null"}, 
+                {"color": "red", "value": 1},
+                {"color": "green", "value": 2},
+                {"color": "red", "value": 3},
+                {"color": "red", "value": 4},
+            ],
+            "upsOutputSource" : [
+                {"color": "gray", "value": "null"}, 
+                {"color": "gray", "value": 1},
+                {"color": "red", "value": 2},
+                {"color": "green", "value": 3},
+                {"color": "red", "value": 4},
+                {"color": "red", "value": 5},
+                {"color": "red", "value": 6},
+                {"color": "red", "value": 7},
+            ],
+        }
+        return r
+
+    def resolve_object_data(self, object):
+        object_metrics = []
+        if self.object.object_profile.report_ping_rtt:
+            object_metrics += ["rtt"]
+
+        om = []
+        for metrics in self.object.object_profile.metrics or []:
+            mt = MetricType.get_by_id(metrics["metric_type"])
+            if not mt:
+                continue
+            om += [mt.name]
+        object_metrics.extend(sorted(om))
+
+        o = Object.get_managed(self.object.id).first() or None
+        if not o:
+            return {
+                "object_metrics": object_metrics,
+            }
+
+        value_mappings = self.get_value_mappings()
+        value_thresholds = self.get_value_thresholds()
+        sensor_enum = []
+
+        sensors = {}
+        for s in Sensor.objects.filter(object=o):
+            sbname = s.local_id.split(".")[0]
+            if not sensors.get(sbname):
+                sensors[sbname] = []
+
+            sensors[sbname].append(s)
+
+        graphs = {}
+        for sbname in sensors:
+            sns = sensors[sbname]
+            s = sns[0]
+            if s.units.enum:
+                sensor_enum += [{"bi_id": s.bi_id, "local_id": s.local_id, "units": s.units, "mappings": value_mappings.get(s.local_id, {}), "thresholds": value_thresholds.get(s.local_id, {}) }]
+                continue
+
+            if len(sns) == 1:
+                graphs[sbname] = {
+                    "label": s.dashboard_label or s.label,
+                    "units": s.units,
+                    "bi_id": s.bi_id,
+                    "local_id": s.local_id,
+                    "profile": s.profile,
+                    "id": int(str(s.bi_id)[-10:]),
+                }
+                continue
+
+            graphs[sbname] = {
+                "label": sns[0].dashboard_label or sns[0].label,
+                "units": sns[0].units,
+                "local_id": sbname,
+                "profile": sns[0].profile,
+                "series": [{
+                    "label": s.dashboard_label or s.label,
+                    "units": s.units,
+                    "bi_id": s.bi_id,
+                    "local_id": s.local_id,
+                    "profile": s.profile,
+                    "id": int(str(s.bi_id)[-10:]),
+                } for s in sns],
+                "id": int(str(sns[0].bi_id)[-10:]),
+            }
+
+        return {
+            "object_metrics": object_metrics,
+            "sensor_enum": sensor_enum,
+            "graphs": graphs,
+        }
+
+        return 
+
+        sensor_types = defaultdict(list)
+        sensor_data = defaultdict(list)
+        sensor_enum = []
+
+        sensor_combo = {}
+
+        for s in Sensor.objects.filter(object=o):
+            s_type = s.profile.name
+            if not s.state.is_productive:
+                s_type = "missed"
+            if s.units.enum and s.state.is_productive:
+                sensor_enum += [{"bi_id": s.bi_id, "local_id": s.local_id, "units": s.units, "mappings": value_mappings.get(s.local_id, {}), "thresholds": value_thresholds.get(s.local_id, {}) }]
+                continue
+            if s.local_id.split(".")[0] in ("upsInputVoltage", "upsInputCurrent"):
+                sensor_combo[s.local_id] = {
+                    "label": s.dashboard_label or s.label,
+                    "units": s.units,
+                    "bi_id": s.bi_id,
+                    "local_id": s.local_id,
+                    "profile": s.profile,
+                    "id": int(str(s.bi_id)[-10:]),
+                }
+                continue
+
+            sensor_types[s_type] += [
+                {
+                    "label": s.dashboard_label or s.label,
+                    "units": s.units,
+                    "bi_id": s.bi_id,
+                    "local_id": s.local_id,
+                    "profile": s.profile,
+                    "id": int(str(s.bi_id)[-10:]),
+                }
+            ]
+        combo = [
+            {
+                "label": "Voltage",
+                "units": sensor_combo["upsInputVoltage.1"]["units"],
+                "bi_id": sensor_combo["upsInputVoltage.1"]["bi_id"],
+                "local_id": sensor_combo["upsInputVoltage.1"]["local_id"],
+                "profile": sensor_combo["upsInputVoltage.1"]["profile"],
+                "series": [sensor_combo[x] for x in sensor_combo if x.split(".")[0] in ("upsInputVoltage")],
+                "id": int(str(sensor_combo["upsInputVoltage.1"]["bi_id"])[-10:]),
+            },
+            {
+                "label": "Current",
+                "units": sensor_combo["upsInputCurrent.1"]["units"],
+                "bi_id": sensor_combo["upsInputCurrent.1"]["bi_id"],
+                "local_id": sensor_combo["upsInputCurrent.1"]["local_id"],
+                "profile": sensor_combo["upsInputCurrent.1"]["profile"],
+                "series": [sensor_combo[x] for x in sensor_combo if x.split(".")[0] in ("upsInputCurrent")],
+                "id": int(str(sensor_combo["upsInputCurrent.1"]["bi_id"])[-10:]),
+            },
+#            {
+#                "label": "batteryRemainingCapacityValue + batteryTotalCapacityValue",
+#                "units": sensor_combo["batteryRemainingCapacityValue"]["units"],
+#                "bi_id": sensor_combo["batteryRemainingCapacityValue"]["bi_id"],
+#                "local_id": sensor_combo["batteryRemainingCapacityValue"]["local_id"],
+#                "profile": sensor_combo["batteryRemainingCapacityValue"]["profile"],
+#                "series": [sensor_combo[x] for x in sensor_combo if x in ("batteryRemainingCapacityValue", "batteryTotalCapacityValue")],
+#                "id": int(str(sensor_combo["batteryRemainingCapacityValue"]["bi_id"])[-10:]),
+#            },
+        ]
+
+        return {
+            "object_metrics": object_metrics,
+            "sensor_enum": sensor_enum,
+            "sensor_types": sensor_types,
+            "sensor_combo": combo,
+        }
+
+    def get_context(self):
+        o_data = self.object_data
+        graphs = self.object_data["graphs"]
+#        sens_types = self.object_data["sensor_types"]
+#        sens_enum = self.object_data["sensor_enum"]
+#        sens_combo = self.object_data["sensor_combo"]
+
+        return {
+#            "port_types": self.object_data["port_types"],
+#            "groups": self.object_data["groups"],
+#            "channels": self.object_data["channels"],
+            "object_metrics": self.object_data["object_metrics"],
+            "device": self.object.name.replace('"', ""),
+            "ip": self.object.address,
+            "platform": self.object.platform.name if self.object.platform else "Unknown platform",
+            "device_id": self.object.id,
+            "firmare_version": self.object.version.version if self.object.version else None,
+            "segment": self.object.segment.id,
+            "vendor": self.object.vendor or "Unknown version",
+#            "sensor_types": self.object_data["sensor_types"],
+            "sensor_enum": self.object_data["sensor_enum"],
+#            "sensor_combo": self.object_data["sensor_combo"],
+            "graphs": self.object_data["graphs"],
+            "bi_id": self.object.bi_id,
+            "pool": self.object.pool.name,
+#            "extra_template": self.extra_template,
+#            "extra_vars": self.extra_vars,
+            "selected_types": defaultdict(list),
+            "ping_interval": self.object.object_profile.ping_interval,
+            "discovery_interval": int(self.object.object_profile.periodic_discovery_interval / 2),
+        }
