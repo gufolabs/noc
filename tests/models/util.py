889af2c5b57cc4c80d718cd074d8b39caaf3d952
Dmitry Volodin <dv@nocproject.org>
2026-07-05 20:45:36 +0200

    tests: Replace model generators for @parametrize with lists (#318)

--- tests/models/util.py
+++ tests/models/util.py
@@ -1,29 +1,33 @@
 # ----------------------------------------------------------------------
 # Various utilities
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2026 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+from functools import cache
+
+
 # NOC modules
-from noc.models import get_model, iter_model_id, is_document
+from noc.models import get_model, iter_model_id, is_document, DB_MODEL_TYPE
 
 
-def get_models():
-    for model_id in iter_model_id():
-        model = get_model(model_id)
-        if model and not is_document(model):
-            yield model
+@cache
+def get_models() -> list[DB_MODEL_TYPE]:
+    return [model for model in get_all_models() if not is_document(model)]
 
 
+@cache
 def get_documents():
-    for model_id in iter_model_id():
-        model = get_model(model_id)
-        if model and is_document(model):
-            yield model
+    return [model for model in get_all_models() if is_document(model)]
 
 
+@cache
 def get_all_models():
+    r: list[DB_MODEL_TYPE] = []
     for model_id in iter_model_id():
         model = get_model(model_id)
-        yield model
+        if model:
+            r.append(model)
+    return r
