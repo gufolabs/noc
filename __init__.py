29a250e24a21efb5179863308569d310019a2e03
Dmitry Volodin <dv@nocproject.org>
2026-07-19 08:22:59 +0200

    Replace VERSIOB file with __version__ attribute (#373)

--- __init__.py
+++ __init__.py
@@ -3,9 +3,20 @@
 # ----------------------------------------------------------------------
 # TESTS: ALLOW_NON_EMPTY_INIT
 # ----------------------------------------------------------------------
-#  Copyright (C) 2007-2025 The NOC Project
+#  Copyright (C) 2007-2026 The NOC Project
 #  See LICENSE for details
 # ----------------------------------------------------------------------
 
+"""
+NOC main module.
+
+Attributes:
+    __version__: Current version.
+"""
+
 # Activate custom module loaders
 import noc.core.importer  # noqa
+
+__version__ = "25.1"
+
+__all__ = ["__version__"]
