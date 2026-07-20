f69b274481cd0153a5fa468d1b683fc16c5d83f3
Dmitry Lukhtionov <dmitryluhtionov@gmail.com>
2026-07-20 17:25:18 +0300

    Update commit

--- sa/profiles/BDCOM/xPON/get_capabilities.py
+++ sa/profiles/BDCOM/xPON/get_capabilities.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
-# BDCOM.xPON.get_capabilities.ex
+# BDCOM.xPON.get_capabilities
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2026 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -15,18 +15,20 @@ class Script(BaseScript):
 
     @false_on_cli_error
     def has_lldp_cli(self):
-        """
-        Check box has lldp enabled
-        """
-        r = self.cli("show lldp interface")
+        #  Check box has lldp enabled
+        r = self.cli("show lldp neighbors")
         return "LLDP is not enabled" not in r
 
     @false_on_cli_error
     def has_stp_cli(self):
-        """
-        Check box has stp enabled
-        """
+        #  Check box has stp enabled
         r = self.cli("show spanning-tree")
         return not (
             "No spanning tree instance exists." in r or "No spanning tree instances exists." in r
         )
+
+    def execute_platform_cli(self, caps):
+        caps["Network | PON | OLT"] = True
+
+    def execute_platform_snmp(self, caps):
+        caps["Network | PON | OLT"] = True
