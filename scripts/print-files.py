702d116408ef84710b7721551bff339f2ae4b914
Dmitry Volodin <dv@nocproject.org>
2026-07-20 19:40:26 +0200

    print-files utility (#390)

---
+++ scripts/print-files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# ----------------------------------------------------------------------
+# print-files script
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2025 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+"""Print all files and directories specified by arguments."""
+
+# Python modules
+from pathlib import Path
+from typing import Iterable
+import sys
+
+HEADER_LEN = 72
+
+
+def iter_files(paths: Iterable[str]) -> Iterable[Path]:
+    for arg in paths:
+        path = Path(arg)
+        if path.is_dir():
+            yield from (p for p in path.rglob("*") if p.is_file())
+        elif path.is_file():
+            yield path
+
+
+def main(*args: str) -> None:
+    paths = args if args else (str(Path.cwd()),)
+    for path in iter_files(paths):
+        header = f"===[ File: {path} ]"
+        if len(header) < HEADER_LEN:
+            header += "=" * (HEADER_LEN - len(header))
+        print(header)
+        print(path.read_text())
+        print()
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
