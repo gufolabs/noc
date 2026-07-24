ef2f7afe53f40d97a70643f3035e2dfa03b96b3e
uncol <d.a.zakharov@gmail.com>
2026-07-24 08:05:15 +0300

    Refactor: update LookupField components

--- ui/web/main/ref/soform/LookupField.js
+++ ui/web/main/ref/soform/LookupField.js
@@ -1,15 +1,27 @@
 //---------------------------------------------------------------------
 // NOC.main.soform.Lookup
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2020 The NOC Project
+// Copyright (C) 2007-2026 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC.main.soform.LookupField");
 
 Ext.define("NOC.main.ref.soform.LookupField", {
-  extend: "NOC.core.ComboBox",
+  extend: "Ext.form.field.ComboBox",
   alias: "widget.main.ref.soform.LookupField",
   uiStyle: "medium-combo",
-  pageSize: false,
   askPermission: false,
+  queryMode: "local",
+  forceSelection: true,
+  editable: false,
+  valueField: "id",
+  displayField: "name",
+  store: {
+    fields: ["id", "name", "icon"],
+    data: [
+      {id: "c", name: __("Circle"), icon: "fa-circle"},
+      {id: "s", name: __("Square"), icon: "fa-square"},
+    ],
+  },
+  tpl: '<tpl for="."><div class="x-boundlist-item"><i class="fa {[values.icon]}"></i>&nbsp; {[values.name]}</div></tpl>',
 });
