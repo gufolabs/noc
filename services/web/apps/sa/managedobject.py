03cab0536104102054cfbf832e24ae61ba899ad0
Dmitry Volodin <dv@nocproject.org>
2026-07-25 11:18:48 +0200

    Restore api_list_full

--- services/web/apps/sa/managedobject.py
+++ services/web/apps/sa/managedobject.py
@@ -1310,6 +1310,14 @@ class ManagedObjectApplication(ExtModelApplication):
             ]
         return r
 
+    @view(url=r"^full/", method=["POST"], access="read", api=True)
+    def api_list_full(self, request: HttpRequest):
+        try:
+            return self.list_data(request, self.instance_to_dict)
+        except Exception as e:
+            error_report()
+            return self.response({"status": False, "message": str(e)}, status=self.INTERNAL_ERROR)
+
     @view(url=r"^list/", method=["POST"], access="read", api=True)
     def api_list_short(self, request: HttpRequest):
         try:
