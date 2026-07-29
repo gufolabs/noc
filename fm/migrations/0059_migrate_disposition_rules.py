f8dd466bb83d77dc615dc4249370d65a7fc9ebaf
Dmitry Volodin <dv@nocproject.org>
2026-07-29 06:38:30 +0200

    Refactor fm.0059_migrate_disposition_rules (#452)
    (cherry picked from commit 2547b5aade1702cc52999fb7c9a7715521066d7c)

--- fm/migrations/0059_migrate_disposition_rules.py
+++ fm/migrations/0059_migrate_disposition_rules.py
@@ -1,12 +1,13 @@
 # ----------------------------------------------------------------------
 # Move Event Class handlers to Event Disposition Rule
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2025 The NOC Project
+# Copyright (C) 2007-2026 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # Python modules
 import uuid
+from typing import Any
 
 # Third-party modules
 import bson
@@ -14,7 +15,7 @@ from pymongo import InsertOne
 
 # NOC modules
 from noc.core.migration.base import BaseMigration
-from noc.core.bi.decorator import bi_hash
+from noc.core.bi.decorator import new_bi_id
 
 interaction_map = {
     "log_cmd": 0,
@@ -28,34 +29,61 @@ interaction_map = {
     "on_config_change": 6,
 }
 discovery_funcs = {"on_system_start", "on_config_change", "schedule_discovery"}
+action_map = {"raise": "R", "clear": "C", "ignore": "I", "drop": "I"}
 
 
 class Migration(BaseMigration):
-    def migrate(self):
+    @staticmethod
+    def get_object_actions(handlers: list[str] | None) -> dict[str, Any] | None:
+        if not handlers:
+            return None
+        interaction = None
+        discovery = False
+        for h in handlers:
+            _, function = h.rsplit(".", 1)
+            if function in interaction_map:
+                interaction = interaction_map[function]
+            if function in discovery_funcs:
+                discovery = True
+        if interaction is None and not discovery:
+            return None
+        return {
+            "interaction_audit": interaction,
+            "run_discovery": discovery,
+        }
+
+    def migrate(self) -> None:
         bulk = []
-        names = set()
-        ac_map = {}
-        for ac in self.mongo_db["noc.alarmclasses"].find({}, {"name": 1}):
-            ac_map[ac["_id"]] = ac["name"]
-        for ec in self.mongo_db["noc.eventclasses"].find():
-            if not ec.get("disposition") and not ec.get("handlers"):
-                continue
-            disposition_names = []
+        seen: set[str] = set()
+        ac_map = {
+            ac["_id"]: ac["name"] for ac in self.mongo_db["noc.alarmclasses"].find({}, {"name": 1})
+        }
+        for ec in self.mongo_db["noc.eventclasses"].find(
+            {
+                "$or": [
+                    {"disposition.0": {"$exists": True}},
+                    {"handlers.0": {"$exists": True}},
+                ]
+            },
+            {"name": 1, "disposition": 1, "handlers": 1},
+        ):
+            disposition_names: dict[str, int] = {}
             for d in ec["disposition"] or []:
                 ac = d.get("alarm_class")
                 if not ac:
                     continue
-                if disposition_names and d["name"] in disposition_names:
-                    name = f"{ec['name']} ({ac_map.get(ac)},{d['name']}) ({disposition_names.index(d['name'])})"
+                idx = disposition_names.get(d["name"], 0)
+                if idx:
+                    name = f"{ec['name']} ({ac_map.get(ac)},{d['name']}) ({idx})"
                 else:
                     name = f"{ec['name']} ({ac_map.get(ac)},{d['name']})"
-                disposition_names.append(d["name"])
-                if name in names:
+                disposition_names[d["name"]] = idx + 1
+                if name in seen:
                     continue
-                da = {"raise": "R", "clear": "C", "ignore": "I", "drop": "I"}.get(d["action"], "I")
+                da = action_map.get(d["action"], "I")
                 r = {
                     "name": name,
-                    "uuid": uuid.uuid4(),
+                    "uuid": uuid.uuid5(uuid.NAMESPACE_OID, name),
                     "is_active": True,
                     "combo_condition": d["combo_condition"],
                     "combo_window": d.get("combo_window") or 0,
@@ -63,45 +91,25 @@ class Migration(BaseMigration):
                     "alarm_disposition": d.get("alarm_class"),
                     "default_action": da,
                     "conditions": [{"event_class_re": ec["name"]}],
-                    "bi_id": bson.Int64(bi_hash(bson.ObjectId())),
+                    "bi_id": bson.Int64(new_bi_id()),
                 }
-                interaction, discovery = None, False
-                for h in ec.get("handlers") or []:
-                    _, function = h.rsplit(".", 1)
-                    if function in interaction_map:
-                        interaction = interaction_map[function]
-                    if function in discovery_funcs:
-                        discovery = True
-                if interaction is not None or discovery:
-                    r["object_actions"] = {
-                        "interaction_audit": interaction,
-                        "run_discovery": discovery,
-                    }
-                bulk += [InsertOne(r)]
-                names.add(name)
-            if ec["disposition"]:
+                if actions := self.get_object_actions(ec.get("handlers")):
+                    r["object_actions"] = actions
+                bulk.append(InsertOne(r))
+                seen.add(name)
+            if ec.get("disposition"):
                 continue
+            name = f"{ec['name']} (handlers)"
             r = {
-                "name": f"{ec['name']} (handlers)",
-                "uuid": uuid.uuid4(),
+                "name": name,
+                "uuid": uuid.uuid5(uuid.NAMESPACE_OID, name),
                 "is_active": True,
                 "handlers": [],
                 "conditions": [{"event_class_re": ec["name"]}],
-                "bi_id": bson.Int64(bi_hash(bson.ObjectId())),
+                "bi_id": bson.Int64(new_bi_id()),
             }
-            interaction, discovery = None, False
-            for h in ec.get("handlers") or []:
-                _, function = h.rsplit(".", 1)
-                if function in interaction_map:
-                    interaction = interaction_map[function]
-                if function in discovery_funcs:
-                    discovery = True
-            if interaction is not None or discovery:
-                r["object_actions"] = {
-                    "interaction_audit": interaction,
-                    "run_discovery": discovery,
-                }
-            bulk += [InsertOne(r)]
-        coll = self.mongo_db["dispositionrules"]
+            if actions := self.get_object_actions(ec.get("handlers")):
+                r["object_actions"] = actions
+            bulk.append(InsertOne(r))
         if bulk:
-            coll.bulk_write(bulk)
+            self.mongo_db["dispositionrules"].bulk_write(bulk)
