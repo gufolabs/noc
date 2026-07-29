75f7db7afedb0a4ac1041379f11159f675f46b13
Dmitry Volodin <dv@nocproject.org>
2026-07-29 16:41:21 +0200

    fix cli tests

--- tests/scripts/test_cli.py
+++ tests/scripts/test_cli.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------
 # CLI testing
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2025 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -15,22 +15,27 @@ import pytest
 from noc.core.script.base import BaseScript
 from noc.core.script.cli.error import CLIConnectionRefused, CLIAuthFailed
 from noc.sa.interfaces.igetdict import IGetDict
+from noc.config import config
 
-
-SSHD_HOST = "sshd"
-DROPBEAR_HOST = "dropbear"
-TELNETD_HOST = "telnetd"
+SSHD_HOST = config.tests.sshd_host
+SSHD_PORT = config.tests.sshd_port
+DROPBEAR_HOST = config.tests.dropbear_host
+DROPBEAR_PORT = config.tests.dropbear_port
+TELNETD_HOST = config.tests.telnetd_host
+TELNETD_PORT = config.tests.telnetd_port
 TEST_USER = "test"
 TEST_PW = "pw1234567890"
+BROKEN_USER = TEST_USER + "X"
+BROKEN_PW = TEST_PW + "X"
 
 
 class ServiceStub:
     class ServiceConfig:
-        def __init__(self, pool, tos=None):
+        def __init__(self, pool, tos=None) -> None:
             self.pool = pool
             self.tos = tos
 
-    def __init__(self, pool="default"):
+    def __init__(self, pool="default") -> None:
         self.config = self.ServiceConfig(pool=pool)
 
 
@@ -50,45 +55,133 @@ class GetDiagScript(BaseScript):
 
 
 @pytest.mark.parametrize(
-    "proto,host,port,user,password,args,xcls",
+    ("proto", "host", "port", "user", "password", "args", "xcls"),
     [
         # Plain call (ssh)
-        ("ssh", SSHD_HOST, 22, TEST_USER, TEST_PW, {}, None),
+        ("ssh", SSHD_HOST, SSHD_PORT, TEST_USER, TEST_PW, {}, None),
         # Call with args (ssh)
-        ("ssh", SSHD_HOST, 22, TEST_USER, TEST_PW, {"x": 1, "y": 2}, None),
+        (
+            "ssh",
+            SSHD_HOST,
+            SSHD_PORT,
+            TEST_USER,
+            TEST_PW,
+            {"x": 1, "y": 2},
+            None,
+        ),
         # Connection refused
         ("ssh", SSHD_HOST, 1022, TEST_USER, TEST_PW, {}, CLIConnectionRefused),
         # Invalid user (ssh)
-        ("ssh", SSHD_HOST, 22, TEST_USER + "X", TEST_PW, {}, CLIAuthFailed),
+        (
+            "ssh",
+            SSHD_HOST,
+            SSHD_PORT,
+            BROKEN_USER,
+            TEST_PW,
+            {},
+            CLIAuthFailed,
+        ),
         # Invalid password (ssh)
-        ("ssh", SSHD_HOST, 22, TEST_USER, TEST_PW + "X", {}, CLIAuthFailed),
+        (
+            "ssh",
+            SSHD_HOST,
+            SSHD_PORT,
+            TEST_USER,
+            BROKEN_PW,
+            {},
+            CLIAuthFailed,
+        ),
         # Plain call (ssh)
-        ("ssh", DROPBEAR_HOST, 22, TEST_USER, TEST_PW, {}, None),
+        (
+            "ssh",
+            DROPBEAR_HOST,
+            DROPBEAR_PORT,
+            TEST_USER,
+            TEST_PW,
+            {},
+            None,
+        ),
         # Call with args (ssh)
-        ("ssh", DROPBEAR_HOST, 22, TEST_USER, TEST_PW, {"x": 1, "y": 2}, None),
+        (
+            "ssh",
+            DROPBEAR_HOST,
+            DROPBEAR_PORT,
+            TEST_USER,
+            TEST_PW,
+            {"x": 1, "y": 2},
+            None,
+        ),
         # Connection refused
         ("ssh", DROPBEAR_HOST, 1022, TEST_USER, TEST_PW, {}, CLIConnectionRefused),
         # Invalid user (ssh)
-        ("ssh", DROPBEAR_HOST, 22, TEST_USER + "X", TEST_PW, {}, CLIAuthFailed),
+        (
+            "ssh",
+            DROPBEAR_HOST,
+            DROPBEAR_PORT,
+            BROKEN_USER,
+            TEST_PW,
+            {},
+            CLIAuthFailed,
+        ),
         # Invalid password (ssh)
-        ("ssh", DROPBEAR_HOST, 22, TEST_USER, TEST_PW + "X", {}, CLIAuthFailed),
+        (
+            "ssh",
+            DROPBEAR_HOST,
+            DROPBEAR_PORT,
+            TEST_USER,
+            BROKEN_PW,
+            {},
+            CLIAuthFailed,
+        ),
         # Plain call (telnet)
-        ("telnet", TELNETD_HOST, 23, TEST_USER, TEST_PW, {}, None),
+        (
+            "telnet",
+            TELNETD_HOST,
+            TELNETD_PORT,
+            TEST_USER,
+            TEST_PW,
+            {},
+            None,
+        ),
         # Call with args (telnet)
-        ("telnet", TELNETD_HOST, 23, TEST_USER, TEST_PW, {"x": 1, "y": 2}, None),
+        (
+            "telnet",
+            TELNETD_HOST,
+            TELNETD_PORT,
+            TEST_USER,
+            TEST_PW,
+            {"x": 1, "y": 2},
+            None,
+        ),
         # Connection refused
         ("telnet", TELNETD_HOST, 1023, TEST_USER, TEST_PW, {}, CLIConnectionRefused),
         # Invalid user (telnet)
-        ("telnet", TELNETD_HOST, 23, TEST_USER + "X", TEST_PW, {}, CLIAuthFailed),
+        (
+            "telnet",
+            TELNETD_HOST,
+            TELNETD_PORT,
+            BROKEN_USER,
+            TEST_PW,
+            {},
+            CLIAuthFailed,
+        ),
         # Invalid password (telnet)
-        ("telnet", TELNETD_HOST, 23, TEST_USER, TEST_PW + "X", {}, CLIAuthFailed),
+        (
+            "telnet",
+            TELNETD_HOST,
+            TELNETD_PORT,
+            TEST_USER,
+            BROKEN_PW,
+            {},
+            CLIAuthFailed,
+        ),
     ],
 )
 def test_cli(proto, host, port, user, password, args, xcls):
     try:
         address = socket.gethostbyname(host)
     except socket.gaierror:
-        pytest.fail("Cannot resolve host '%s'" % host)
+        pytest.fail("Cannot resolve host '{host}'")
     scr = GetDiagScript(
         service=ServiceStub(),
         credentials={
@@ -111,6 +204,8 @@ def test_cli(proto, host, port, user, password, args, xcls):
     result = scr.run()
     # Perform checks
     assert result
+    print(f"<<<{result['motd']}>>>")
+    print(f">>>{result['cat-motd']}<<<")
     assert result["motd"] == result["cat-motd"]
     assert result["args"] == args
     assert result["fqdn"]
