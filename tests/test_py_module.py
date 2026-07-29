bf0f772d8ea92347904c8028135459e105469bda
Dmitry Volodin <dv@nocproject.org>
2026-07-29 20:06:15 +0200

    do not check src/

--- tests/test_py_module.py
+++ tests/test_py_module.py
@@ -55,16 +55,14 @@ def _allow_xfail(module: str) -> bool:
 def get_files():
     def _get_files():
         try:
-            data = smart_text(
-                subprocess.check_output(["git", "ls-tree", "HEAD", "-r", "--name-only"])
-            )
+            data = subprocess.check_output(["git", "ls-tree", "HEAD", "-r", "--name-only"]).decode()
             return data.splitlines()
         except (OSError, subprocess.CalledProcessError):
             # No git, emulate
-            data = smart_text(subprocess.check_output(["find", ".", "-type", "f", "-print"]))
+            data = subprocess.check_output(["find", ".", "-type", "f", "-print"]).decode()
             return [p[2:] for p in data.splitlines()]
 
-    return [x for x in _get_files() if not x.startswith("docs")]
+    return [x for x in _get_files() if not x.startswith("docs") and no x.startswith("src")]
 
 
 @cachetools.cached(cache={})
